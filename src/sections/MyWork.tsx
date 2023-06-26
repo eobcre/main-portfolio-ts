c26f0385006a7746679e310467a26c5bf210699f
eobcre <eobcre@gmail.com>
2023-06-26 19:37:17 -0400

    button name changed

--- src/sections/MyWork.tsx
+++ src/sections/MyWork.tsx
@@ -37,7 +37,7 @@ const MyWork: React.FC = () => {
                   <span className='tech-st'>{data.tech4}</span>
                   <span className='tech-st'>{data.tech5}</span>
                 </div>
-                <Button name='View' link={data.url} />
+                <Button name='Go To App' link={data.url} />
               </ScrollFadeIn>
             </div>
           </div>

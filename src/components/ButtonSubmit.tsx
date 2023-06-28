35145ade2c8daf36ceeca39fd9fa4ae26229b60a
eobcre <eobcre@gmail.com>
2023-06-28 10:59:05 -0400

    button style changed

--- src/components/ButtonSubmit.tsx
+++ src/components/ButtonSubmit.tsx
@@ -7,7 +7,7 @@ const ButtonSubmit: React.FC<ButtonSubmitProps> = (props) => {
   return (
     <div className='flex justify-center py-8'>
       <button
-        className='bg-dark-gray font-bebas text-white text-2xl hover:text-light-blue rounded-md transition-all px-16 py-2'
+        className='glass font-bebas text-dark-gray text-2xl shadow-showcase-bg hover:text-pink rounded-md transition-all px-16 py-2'
         type='submit'
       >
         <span>{props.name}</span>

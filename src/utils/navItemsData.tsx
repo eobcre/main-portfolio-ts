1bff1e3801e1ab7db90766a052730f388e86a405
eobcre <eobcre@gmail.com>
2023-07-10 18:57:02 -0400

    created nav items data for header

---
+++ src/utils/navItemsData.tsx
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    item: 'About',
+    link: 'About',
+  },
+  {
+    item: 'Work',
+    link: 'Work',
+  },
+  {
+    item: 'Contact',
+    link: 'Contact',
+  },
+  {
+    item: 'Resume',
+    pdf: 'PDF',
+  },
+];

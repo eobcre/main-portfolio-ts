83bda634512fd2a82f2bd870750d9a78b80e61a5
eobcre <eobcre@gmail.com>
2023-09-12 13:00:00 -0400

    updated props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,28 +4,28 @@ type ButtonProps = {
   type?: 'link' | 'submit';
 };
 
-const Button: React.FC<ButtonProps> = (props) => {
+const Button: React.FC<ButtonProps> = ({ name, link, type }) => {
   const buttonClass =
     'glass font-bebas text-dark-gray text-2xl shadow-showcase-bg hover:text-pink rounded-md duration-500 px-16 py-2';
 
-  if (props.type === 'link' && props.link) {
+  if (type === 'link' && link) {
     return (
       <div className='py-10'>
         <a
-          href={props.link}
+          href={link}
           target='_blank'
           rel='noopener noreferrer'
           className={`text-dark-gray text-2xl shadow-showcase-bg glass rounded-md hover:text-pink duration-500 px-12 py-2`}
         >
-          <span className='font-bebas'>{props.name}</span>
+          <span className='font-bebas'>{name}</span>
         </a>
       </div>
     );
-  } else if (props.type === 'submit') {
+  } else if (type === 'submit') {
     return (
       <div className='flex justify-center py-8'>
         <button className={buttonClass} type='submit'>
-          <span>{props.name}</span>
+          <span>{name}</span>
         </button>
       </div>
     );

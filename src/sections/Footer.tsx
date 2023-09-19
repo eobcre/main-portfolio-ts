57b95a97669b958692825e44b77095600cea3fe0
eobcre <eobcre@gmail.com>
2023-09-19 13:17:26 -0400

    updated custom responsive

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <div className='bg-light-blue h-20 md:hidden'>
+    <div className='bg-light-blue h-20 min-[600px]:hidden'>
       <div className='glass h-20'>
         <p className='text-dark-gray text-2xl font-bebas text-center py-6'>
           &copy; 2023 Emmy Manning

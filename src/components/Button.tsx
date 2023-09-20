dd702667d08ac79906d77455247cf67b0279f5e3
eobcre <eobcre@gmail.com>
2023-09-19 20:38:38 -0400

    updated custom font class name

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<ButtonProps> = ({ name, link, type }) => {
           rel='noopener noreferrer'
           className={`text-dark-gray text-2xl shadow-showcase-bg glass rounded-md hover:text-pink duration-500 px-12 py-2`}
         >
-          <span className='font-bebas'>{name}</span>
+          <span className='font-main'>{name}</span>
         </a>
       </div>
     );
@@ -22,7 +22,7 @@ const Button: React.FC<ButtonProps> = ({ name, link, type }) => {
     return (
       <div className='flex justify-center py-8'>
         <button
-          className={`glass font-bebas text-dark-gray text-2xl shadow-showcase-bg hover:text-pink rounded-md duration-500 px-16 py-2`}
+          className={`glass font-main text-dark-gray text-2xl shadow-showcase-bg hover:text-pink rounded-md duration-500 px-16 py-2`}
           type='submit'
         >
           <span>{name}</span>

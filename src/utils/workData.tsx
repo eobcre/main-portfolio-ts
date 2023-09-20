8c8dd1eca7ffadadf9028dc9345f24c796335dee
eobcre <eobcre@gmail.com>
2023-09-19 20:41:42 -0400

    updated description

--- src/utils/workData.tsx
+++ src/utils/workData.tsx
@@ -8,7 +8,7 @@ export const workData = [
     logo: '../../work_assets/movie-app.jpg',
     url: 'https://movie-new-app-c1f4d014fe65.herokuapp.com/',
     description:
-      'Action! Welcome to Lukub, it is a simple movie app built with OMDB API. This is my latest achievement in asynchronous and dark mode features. Login screen is coming soon!',
+      'Action! It is a simple movie app built with OMDB API. This is my latest achievement in asynchronous and dark mode features. Login screen is coming soon.',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'TypeScript',
@@ -22,7 +22,7 @@ export const workData = [
     logo: '../../work_assets/salon.jpg',
     url: 'https://cait-salon.herokuapp.com/',
     description:
-      'The website was made for my friend, who is a hair stylist in San Diego, CA. She has exellent skills and techniques. Please check it out!',
+      'The website was made for my friend, who is a hair stylist in San Diego, California. She has exellent skills and techniques. Please check it out!',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'JavaScript',
@@ -36,7 +36,7 @@ export const workData = [
     logo: '../../work_assets/post-app.jpg',
     url: 'https://sticky-notes-app.herokuapp.com/',
     description:
-      'Discover the functionality of the sticky notes in the app, enabling you to carry your ideas wherever you go!',
+      'Discover the functionality of the sticky notes in the app, enabling you to carry your ideas wherever you go.',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'JavaScript',
@@ -50,7 +50,7 @@ export const workData = [
     logo: '../../work_assets/omikuji.jpg',
     url: 'https://omikuji-fortune-app.herokuapp.com/',
     description:
-      'Omikuji is a paper slip that contains the result of fortune-telling. It is believed to predict your future and is commonly found in shrines and temples throughout Japan. Now, you can try it on an app and find out your fortune!',
+      'Omikuji is a paper slip that contains the result of fortune-telling. It is believed to predict your future and is commonly found in shrines and temples throughout Japan. Now, you can try it on an app and find out your fortune.',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'TypeScript',

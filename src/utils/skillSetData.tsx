5620a3079da77bf36d9d631ff488ecf759127828
eobcre <eobcre@gmail.com>
2023-09-19 20:42:03 -0400

    updated icon and icon component

--- src/utils/skillSetData.tsx
+++ src/utils/skillSetData.tsx
@@ -1,48 +1,71 @@
 import { nanoid } from 'nanoid';
 
+import { Icon } from '@iconify/react';
+
 export const skillSetData = [
   {
     id: nanoid(),
     skill: 'HTML',
+    icon: <Icon icon='icomoon-free:html-five' />,
   },
   {
     id: nanoid(),
     skill: 'CSS',
+    icon: <Icon icon='simple-icons:css3' />,
   },
   {
     id: nanoid(),
     skill: 'Sass',
+    icon: <Icon icon='akar-icons:sass-fill' />,
   },
   {
     id: nanoid(),
     skill: 'Tailwind CSS',
+    icon: <Icon icon='simple-icons:tailwindcss' />,
   },
   {
     id: nanoid(),
     skill: 'JavaScript',
+    icon: <Icon icon='akar-icons:javascript-fill' />,
   },
   {
     id: nanoid(),
     skill: 'TypeScript',
+    icon: <Icon icon='akar-icons:typescript-fill' />,
   },
   {
     id: nanoid(),
     skill: 'Node.js',
+    icon: <Icon icon='fontisto:nodejs' />,
   },
   {
     id: nanoid(),
     skill: 'React',
+    icon: <Icon icon='teenyicons:react-solid' />,
   },
   {
     id: nanoid(),
     skill: 'Git',
+    icon: <Icon icon='bi:git' />,
   },
   {
     id: nanoid(),
     skill: 'GitHub',
+    icon: <Icon icon='bi:github' />,
   },
   {
     id: nanoid(),
     skill: 'Visual Studio',
+    icon: <Icon icon='mdi:microsoft-visual-studio-code' />,
+  },
+  {
+    id: nanoid(),
+    skill: 'Vite',
+    icon: <Icon icon='devicon-plain:vitejs' />,
+  },
+  {
+    id: nanoid(),
+    skill: 'Heroku',
+    icon: <Icon icon='bxl:heroku' />,
   },
 ];

949182e0b111346bd92d689614ecb505d8589fe2
eobcre <eobcre@gmail.com>
2023-09-19 22:01:11 -0400

    updated custom font class name

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
   return (
     <div className='bg-light-blue h-20 min-[600px]:hidden'>
       <div className='glass h-20'>
-        <p className='text-dark-gray text-2xl font-bebas text-center py-6'>
+        <p className='text-dark-gray text-2xl font-main text-center py-6'>
           &copy; 2023 Emmy Manning
         </p>
       </div>

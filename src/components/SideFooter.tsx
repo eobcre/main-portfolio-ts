856ba8940bd344cb1f5d94a776f526ab555f68d7
eobcre <eobcre@gmail.com>
2023-09-19 20:38:06 -0400

    updated custom font class name

--- src/components/SideFooter.tsx
+++ src/components/SideFooter.tsx
@@ -2,7 +2,7 @@ const SideFooter: React.FC = () => {
   return (
     <div>
       <div className='fixed bottom-0 text-dark-gray text-2xl [writing-mode:vertical-rl] [text-orientation:sideways-right] px-4 py-8'>
-        <p className='hidden min-[600px]:block md:text-dark-gray font-bebas'>
+        <p className='hidden min-[600px]:block md:text-dark-gray font-main'>
           &copy; 2023 Emmy Manning
         </p>
       </div>
